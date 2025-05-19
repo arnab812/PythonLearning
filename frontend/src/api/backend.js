9ccccd1c5d0c1885d44046e32ad3e0e064f5ed07
Arnab Chakraborty <arnab.chakraborty@powerschool.com>
2025-05-20 01:37:57 +0530

    Connect Hosted API Base URL

--- frontend/src/api/backend.js
+++ frontend/src/api/backend.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'http://localhost:8000/api';
+export const API_BASE_URL = 'http://13.127.53.86:8000/api';
 
 export const getConfig = async () => {
     try {
